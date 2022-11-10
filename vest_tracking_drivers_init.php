d958b12ed5c8630fd52d32b85841a19431fc4c3a
Will Barbour <12778879+barbourww@users.noreply.github.com>
2022-11-10 17:25:44 -0600

    Update vest_tracking_drivers_init.php

--- vest_tracking_drivers_init.php
+++ vest_tracking_drivers_init.php
@@ -167,6 +167,13 @@
         ?>
 
     </center>
+        
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+        
     </body>
     
 </html>
