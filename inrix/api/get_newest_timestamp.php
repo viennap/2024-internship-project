4c0b5574ae19693d8587c7bb3e1272795c4b3f6c
Alex Richardson <william.a.richardson@vanderbilt.edu>
2022-11-12 12:33:57 -0600

    INRIX timestamp PHP script added. Probably doesn't work.

---
+++ inrix/api/get_newest_timestamp.php
@@ -0,0 +1,53 @@
+<?php
+//declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+$servername = "127.0.0.1";
+$username = "circles";
+$password = "wjytxeu5";
+$db = "circledb";
+
+#echo $username;
+$conn = new mysqli($servername, $username, $password, $db);
+if ($conn->connect_error)
+{
+  echo $conn->connect_error;
+  die("Connection failed: " . $conn->connect_error);
+}
+#echo "Connected successfully\n";
+
+function get_all_data() {
+  global $conn;
+  $result_array = array();
+  if (array_key_exists('timestamp', $_GET)) {
+    $statement = $conn->prepare('SELECT MAX(queried_at) FROM fact_inrix_estimate WHERE queried_at <= ?');
+    $statement->bind_param('d', (double)$_GET['timestamp']);
+    $statement->execute();
+    $result = $statement->get_result();
+    if ($result->num_rows == 1) {
+      $row = $result->fetch_assoc();
+      $result_array['timestamp'] = $row[0];
+    }
+  }
+  else {
+    $statement = "SELECT MAX(queried_at) FROM fact_inrix_estimate";
+    $result = $conn->query($statement);
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $result_array['timestamp'] = $row[0];
+      }
+  }
+  return $result_array;
+}
+
+header("Content-Type: application/json");
+echo json_encode(get_all_data());
+$conn->close();
+?>
+
+
+
+
+
+
