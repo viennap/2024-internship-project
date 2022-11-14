f5816fc5841033c88863aadc102f8dafea8fc8c2
Alex Richardson <william.a.richardson@vanderbilt.edu>
2022-11-14 13:44:13 -0600

    New current map view for GPS bug fix.

--- GPS_REST_API/current_map.php
+++ GPS_REST_API/current_map.php
@@ -64,12 +64,12 @@ if ($result->num_rows > 0)
 
 <style>
 body { margin: 0; padding: 0; }
-#map { position: absolute; top: 0; bottom: 0; width: 100%; }
+#map { position: absolute; top: 10%; bottom: 0; width: 100%; }
 </style>
 </head>
 <body>
-<div id="map"></div>
 <p><?php echo json_encode($coords); ?></p>
+<div id="map"></div>
 
 <script>
 	// TO MAKE THE MAP APPEAR YOU MUST
