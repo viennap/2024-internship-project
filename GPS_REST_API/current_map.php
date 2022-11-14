a985c712d81a3cc3e617db57c51cb5d66317c555
Alex Richardson <william.a.richardson@vanderbilt.edu>
2022-11-14 13:39:04 -0600

    New current map view for GPS bug fix.

--- GPS_REST_API/current_map.php
+++ GPS_REST_API/current_map.php
@@ -1,4 +1,7 @@
 <?php 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 $servername = "127.0.0.1";
 $username = "circles";
 $password = "wjytxeu5";
