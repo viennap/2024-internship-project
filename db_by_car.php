767fc1b4ec97c3c189196987abf077abaf9c5761
MatthewNice <matthewwalternice@gmail.com>
2022-11-16 05:50:36 -0600

    Update db_by_car.php

--- db_by_car.php
+++ db_by_car.php
@@ -115,7 +115,7 @@ if ($result->num_rows > 0) {
       elseif(substr_compare($row[log_message],"nominal state!",0,13)==0 && $row[current_update_min]<1) {
         echo"<td style='background-color: ".$YELLOW.";'>".$row[log_message]." </td>";
       }
-      elseif(substr_compare($row[log_message],"Rsync finished, shutting system down",0,23)==0 && $row[current_update_min]>10) {
+      elseif(substr_compare($row[log_message],"Rsync finished, shutting system down",0,23)==0 && $row[current_update_min]<10) {
         echo"<td style='background-color: ".$YELLOW.";'>".$row[log_message]." </td>";
       }
       elseif(substr_compare($row[log_message],"nominal state - vehicle says: controls allowable!",0,43)==0 && $row[current_update_min]<10) {
