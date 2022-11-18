f0f29c4f167f662ffb37b6216a626fe0ec0a3243
liljonnystyle <jonny5@berkeley.edu>
2022-11-18 07:32:39 -0600

    Update inrix.php

--- inrix.php
+++ inrix.php
@@ -1,12 +1,12 @@
 <html>
 <head>
-<title>The Latest piStatus information --- Details</title>
+<title>INRIX Heatmap</title>
 <meta http-equiv="refresh" content="300" >
 </head>
 <body>
 <center>
 
-<?php
+<!-- <?php
 // open the file in a binary mode
 $name = '/isis/home/hanw/circles/visual/by_date/20221118/fig/0-heatmap.png';
 $fp = fopen($name, 'rb');
@@ -18,7 +18,7 @@ header("Content-Length: " . filesize($name));
 // dump the picture and stop the script
 fpassthru($fp);
 exit;
-?>
+?> -->
   
 </center>
 </body>
