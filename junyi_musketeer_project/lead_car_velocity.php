89dd89219f8c32ae058b11ce28974c2f112c1924
Alex Richardson <william.a.richardson@vanderbilt.edu>
2023-11-14 15:45:32 -0600

    Created endpoint with template for lead car velocity.

--- junyi_musketeer_project/lead_car_velocity.php
+++ junyi_musketeer_project/lead_car_velocity.php
@@ -5,5 +5,17 @@ $username = "circles";
 $password = "wjytxeu5";
 $db = "junyi_musketeer_team";
 
+$conn = new mysqli($servername, $username, $password, $db);
+if ($conn->connect_error)
+{
+				echo $conn->connect_error;
+				die("Connection failed: " . $conn->connect_error);
+}
+
+function getLeadVin() {
+	return "4";
+}
+
+echo getLeadVin();
 
 ?>
