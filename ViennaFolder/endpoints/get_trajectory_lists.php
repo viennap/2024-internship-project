dc1321d54be5aa9cb6f41f9b3ecf8535c4bc8b97
viennap <22viennap@gmail.com>
2024-06-11 15:10:30 -0500

    Modified for each directory

--- ViennaFolder/endpoints/get_trajectory_lists.php
+++ ViennaFolder/endpoints/get_trajectory_lists.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 $dict = array(); 
 
 if (array_key_exists("start_time", $_GET) and array_key_exists("end_time", $_GET)) {
-    $trajectory_id = "asdf";
+    $trajectory_id = "libpanda/2021_01_04";
     $start_time = 1701389314.4;
     $end_time = 1701389496.6;
 
@@ -21,9 +21,40 @@ if (array_key_exists("start_time", $_GET) and array_key_exists("end_time", $_GET
     $dict["trajectories"][$trajectory_id]["end_time"] = $end_time; 
     $dict["trajectories"][$trajectory_id]["latitude"] = $latitude;
     $dict["trajectories"][$trajectory_id]["longitude"] = $longitude;
+
+    $base_dir = "/volume1/ViennaData/NonDashcamData/libpanda";
+    $directories = scandir($base_dir);
+
+    // Iterate through the "libpanda" folder and create new 
+    // trajectory_id => (trajectory_id, start_time, ...) entry, where
+    // trajectory_id is relative path of folder (e.g., "libpanda/2021_01_04")
     
+    foreach ($directories as $directory) {
+        if ($directory !== '.' && $directory !== '..') {
+            $trajectory_path = "$base_dir/$directory";
+            if (is_dir($trajectory_path)) {
+                $trajectory_id = "libpanda/$directory";
+
+                $dict["trajectories"][$trajectory_id]["id"] = $trajectory_id; 
+                $dict["trajectories"][$trajectory_id]["start_time"] = $start_time; 
+                $dict["trajectories"][$trajectory_id]["end_time"] = $end_time; 
+                $dict["trajectories"][$trajectory_id]["latitude"] = $latitude;
+                $dict["trajectories"][$trajectory_id]["longitude"] = $longitude;
+                
+                /*
+                $dict["trajectories"][$trajectory_id] = array(
+                    "id" => $trajectory_id,
+                    "start_time" => $start_time, 
+                    "end_time" => $end_time,
+                    "latitude" => $latitude,
+                    "longitude" => $longitude
+                );*/
+            }
+        }
+    }
+
     // array_push($dict["trajectories"][$trajectory_id], $latitude, $longitude, );
-   
+
     $result = $dict; 
 
     header("Content-Type: application/json");
