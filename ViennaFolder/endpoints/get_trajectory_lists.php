26c04f1fc1fa86c02baa4c4ac0981f3ebb0e1f6d
viennap <22viennap@gmail.com>
2024-06-11 11:00:04 -0500

    Trajectory lists

--- ViennaFolder/endpoints/get_trajectory_lists.php
+++ ViennaFolder/endpoints/get_trajectory_lists.php
@@ -18,7 +18,9 @@ if (array_key_exists("start_time", $_GET) and array_key_exists("end_time", $_GET
     $dict["trajectories"] = array(); 
     $dict["trajectories"][$trajectory_id] = array();
     array_push($dict["trajectories"][$trajectory_id], $start_time, $end_time);
+    $dict["trajectories"]["abcd"] = array(); 
     array_push($dict["trajectories"]["abcd"], $start_time, $end_time);
+    $dict["trajectories"]["efgh"] = array(); 
     array_push($dict["trajectories"]["efgh"], $start_time, $end_time);
 
     $result = $dict; 
