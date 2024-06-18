2b7cbaf5eef594c390f37c5d940f9e8d729670f1
viennap <22viennap@gmail.com>
2024-06-18 13:40:04 -0500

    Attempting to update build

--- ViennaFolder/driving-dashboard/src/index.js
+++ ViennaFolder/driving-dashboard/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import VehicleTrajectory from './VehicleTrajectory';
-import VehicleSteer from './VehicleSteer'
 
 ReactDOM.render(
   <React.StrictMode>
     <VehicleTrajectory />
-    {/* <VehicleSteer /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
