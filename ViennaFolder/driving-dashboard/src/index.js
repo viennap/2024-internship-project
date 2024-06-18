b4c5c334107049857fc75c9b8d47d18dba3c0549
viennap <22viennap@gmail.com>
2024-06-18 13:20:18 -0500

    Updates

--- ViennaFolder/driving-dashboard/src/index.js
+++ ViennaFolder/driving-dashboard/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import App from './App';
+import VehicleTrajectory from './VehicleTrajectory';
+import VehicleSteer from './VehicleSteer'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <VehicleTrajectory />
+    {/* <VehicleSteer /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
