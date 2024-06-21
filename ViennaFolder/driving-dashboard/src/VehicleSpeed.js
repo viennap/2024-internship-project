2639187d4fca3b640a9b03d7e079338f8b20e685
viennap <22viennap@gmail.com>
2024-06-21 11:27:01 -0500

    Adding src files

---
+++ ViennaFolder/driving-dashboard/src/VehicleSpeed.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { Line } from "react-chartjs-2";
+import { CategoryScale } from 'chart.js';
+import Chart from "chart.js/auto";
+import "./styles.css";
+
+Chart.register(CategoryScale);
+
+export default function VehicleSpeed() {
+  const [chartData, setChartData] = useState({ labels: [], datasets: [] });
+
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://ransom.isis.vanderbilt.edu/ViennaFolder/endpoints_python/get_vehicle_signal?signal_name=speed&trajectory_id=2021-01-04-22-36-11_2T3Y1RFV8KC014025');
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const parsed = JSON.parse(xhr.responseText);
+        console.log(parsed);
+        console.log(parsed['time']);
+
+        const data = {
+          labels: parsed['time'],
+          datasets: [{
+            label: 'Wheel Speed',
+            data: parsed['signal'],
+            borderWidth: 1
+          }]
+        };
+
+        setChartData(data);
+      } else {
+        console.log('Error fetching data.');
+      }
+    };
+    xhr.send();
+  }, []);
+
+  return (
+    <div className="chart-container">
+      <h2 style={{ textAlign: "center" }}>Wheel Speed Over Time</h2>
+      <Line
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+            },
+            legend: {
+              display: true,
+              position: 'top'
+            }
+          }
+        }}
+      />
+    </div>
+  );
+}
+
