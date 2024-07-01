1a8c3458eb0d91984371b9b28d0e899dd6183674
Alex Richardson <william.a.richardson@vanderbilt.edu>
2024-07-01 16:33:49 -0500

    Fixed the routes plotting before the map is ready

--- ViennaFolder/driving-dashboard/src/Map.js
+++ ViennaFolder/driving-dashboard/src/Map.js
@@ -6,29 +6,30 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoidmllbm5hcCIsImEiOiJjbHg5NjR4cWgwbjB4MmtwajRlZ
 export default function Map({trajectoryList, selectedTrajectoryId}){
     const mapContainer = useRef(null);
     const map = useRef(null);
+    const [mapReady, setMapReady] = useState(false);
 
     useEffect(() => {
         if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
+        let newMap = new mapboxgl.Map({
           container: mapContainer.current,
           style: 'mapbox://styles/mapbox/streets-v12',
           center: [-86.767960, 36.174465],
           zoom: 5
         });
 
-        map.current.on('load', async () => {
+        newMap.on('load', async () => {
             let GeoJSON = {};
 
             GeoJSON["features"] = [];
             GeoJSON["properties"] = {};
             GeoJSON["type"] = "FeatureCollection";
 
-            map.current.addSource('my-route', {
+            newMap.addSource('my-route', {
                 "type": "geojson",
                 "data": GeoJSON
             });
 
-            map.current.addLayer({
+            newMap.addLayer({
                 id: 'my-route-layer',
                 source: 'my-route',
                 type: 'line',
@@ -42,44 +43,46 @@ export default function Map({trajectoryList, selectedTrajectoryId}){
                     'line-opacity': 0.8,
                 }
             });
+            setMapReady(true);
         });
+        map.current = newMap;
     });
 
     useEffect(() => {
-        let GeoJSON = {};
-        GeoJSON["features"] = [];
-        GeoJSON["properties"] = {};
-        GeoJSON["type"] = "FeatureCollection";
+        if (mapReady) {
+            let GeoJSON = {};
+            GeoJSON["features"] = [];
+            GeoJSON["properties"] = {};
+            GeoJSON["type"] = "FeatureCollection";
 
-        console.log(GeoJSON);
+            console.log(GeoJSON);
 
-        for (const [id, trajectory] of Object.entries(trajectoryList)) {
-            let route = {};
-            route["type"] = "Feature";
-            route["properties"] = {};
-            route["geometry"] = {};
-            route["geometry"]["type"] = "MultiLineString";
-            route["geometry"]["coordinates"] = [];
+            for (const [id, trajectory] of Object.entries(trajectoryList)) {
+                let route = {};
+                route["type"] = "Feature";
+                route["properties"] = {};
+                route["geometry"] = {};
+                route["geometry"]["type"] = "MultiLineString";
+                route["geometry"]["coordinates"] = [];
 
-            let currentTrajectory = [];
-            for (let i = 0; i < trajectory['latitude'].length; i++) {
-                let pair = [trajectory['longitude'].at(i), trajectory['latitude'].at(i)];
-                currentTrajectory.push(pair);
+                let currentTrajectory = [];
+                for (let i = 0; i < trajectory['latitude'].length; i++) {
+                    let pair = [trajectory['longitude'].at(i), trajectory['latitude'].at(i)];
+                    currentTrajectory.push(pair);
+                }
+
+                route["geometry"]["coordinates"].push(currentTrajectory);
+
+                let color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+                
+                route["properties"]["myColorProperty"] = color;
+
+                GeoJSON["features"].push(route);
             }
-
-            route["geometry"]["coordinates"].push(currentTrajectory);
-
-            let color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-            
-            route["properties"]["myColorProperty"] = color;
-
-            GeoJSON["features"].push(route);
+            map.current.getSource('my-route').setData(GeoJSON);
         }
 
-
-        map.current.getSource('my-route').setData(GeoJSON);
-
-    }, [trajectoryList, selectedTrajectoryId]);
+    }, [mapReady, trajectoryList, selectedTrajectoryId]);
 
     return (
         <div ref={mapContainer} className="map-container" style= {{ height: '500px' }}></div>
