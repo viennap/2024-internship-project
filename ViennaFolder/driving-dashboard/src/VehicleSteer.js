0f04a40faa438ff2f5ba1e981d97c0d4bd2aab04
viennap <22viennap@gmail.com>
2024-07-11 15:09:43 -0500

    Annotation plugin

--- ViennaFolder/driving-dashboard/src/VehicleSteer.js
+++ ViennaFolder/driving-dashboard/src/VehicleSteer.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Line} from "react-chartjs-2";
 import { CategoryScale } from 'chart.js';
-import Chart from "chart.js/auto";
 import "./styles.css";
-
-import { Annotation } from 'chartjs-plugin-annotation';
-// Chart.register(Annotation);
+import { Chart } from 'chart.js';
+import { annotationPlugin } from 'chartjs-plugin-annotation';
+Chart.register(annotationPlugin);
 
 Chart.register(CategoryScale);
 
